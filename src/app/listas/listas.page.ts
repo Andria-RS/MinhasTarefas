dd8af3c3eae595ceff36e37925bd470e82d9919d
Andria-RS <andria2034@gmail.com>
2025-10-14 14:56:23 +0100

    Fase2_Ponto1;2;3;4;5

--- src/app/listas/listas.page.ts
+++ src/app/listas/listas.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-listas',
@@ -8,7 +9,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListasPage implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
+  
+  irPaginaTarefas() {
+    this.router.navigateByUrl('/tarefas'); 
+  }
 
   ngOnInit() {
   }
