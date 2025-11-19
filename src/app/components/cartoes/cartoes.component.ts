2b499e62c815155dca570fda1e0b8b3013062e4c
Andria-RS <andria2034@gmail.com>
2025-11-19 15:57:30 +0000

    Reorder

--- src/app/components/cartoes/cartoes.component.ts
+++ src/app/components/cartoes/cartoes.component.ts
@@ -16,14 +16,12 @@ export class CartoesComponent  implements OnInit {
   }
 
   eliminarTarefa(id: number) {
-  this.tasks = this.tasks.filter(item => item.id !== id);
+    this.tasks = this.tasks.filter(item => item.id !== id);
   }
 
   reordenacao(event: any) {
     const movedItem = this.tasks.splice(event.detail.from, 1)[0];
-
     this.tasks.splice(event.detail.to, 0, movedItem);
-
     event.detail.complete();
   }
 
